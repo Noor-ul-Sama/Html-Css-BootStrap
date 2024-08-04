99a323f81474194504edd8075c914962b6cbff65
Noor-ul-Sama <noorulsama25@gmail.com>
2024-08-04 11:08:22 +0500

    Add files via upload

---
+++ ScriPt.js
@@ -0,0 +1,13 @@
+const bar = document.getElementById('bar');
+const close= document.getElementById('close');
+const nav = document.getElementById('navbar');
+if(close){
+        close.addEventListener('click', function () {
+            navbar.classList.remove('active');
+        });
+        }
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
